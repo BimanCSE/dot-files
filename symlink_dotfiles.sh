2403a3e230c42267495f8818e953d1496f72b8b6
BimanCSE <biman20006@iiitd.ac.in>
2023-08-07 11:13:49 +0000

    some prequiresites in sh file

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,4 +1,9 @@
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/.vimrc ~/.vimrc
-
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+brew install vim
+python3 -m pip install jedi
+brew install black
+npm install -g yarn
+python3 -m pip install black
